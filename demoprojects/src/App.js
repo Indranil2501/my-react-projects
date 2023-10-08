cc77138c6d136e6b482a83dfcaa7c7f202390421
indranil2501 <indranil2501@gmail.com>
2023-10-08 17:36:25 +0530

    Added bootstrap and Popover

--- demoprojects/src/App.js
+++ demoprojects/src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Components/Home';
+import Home from './components/Home';
 
 function App() {
   return (
--- demoprojects/src/Components/Home.js
+++ demoprojects/src/Components/Home.js
@@ -1,20 +1,50 @@
+import { Card, CardActions, CardContent, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
 import React, { useState } from 'react';
-import Popover from './Popover';
+import Popover from '@mui/material/Popover';
 
 const Home = () => {
   const [popoverOpen, setPopoverOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const handleClose = () => {
+    setAnchorEl(null);
+    onClose();
+  };
   const togglePopover = () => {
     setPopoverOpen(!popoverOpen);
   };
 
   return (
-    <div className="card">
-      <div className="card-content" onClick={togglePopover}>
-        Click to Open Popover
+    <>
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'center',
+        }}
+      >
+        <div className="popover-content">
+          <div className="popover-header">Popover Header</div>
+          <div className="popover-body">Popover Body Content</div>
+          <Button onClick={handleClose}>Close Popover</Button>
+        </div>
+      </Popover>
+      <div className="card m-1 p-1">
+        <Button onClick={togglePopover}>
+          Open Popover
+        </Button>
+        {popoverOpen && <PopoverComponent onClose={togglePopover} />}
       </div>
-      {popoverOpen && <Popover onClose={togglePopover} />}
-    </div>
+      <div className="card m-1 p-1">
+      </div>
+    </>
   );
 };
 
